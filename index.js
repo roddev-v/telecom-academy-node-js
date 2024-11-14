c294fb1c51125488ad493ad84531482b88930d68
Daniel Iovescu <iovescu.dani@gmail.com>
2024-11-14 18:37:36 +0200

    work in progress - nodejs to express migration

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-const jokes = require('give-me-a-joke');
+const express = require("express");
+const app = express();
 
-jokes.getRandomDadJoke(function(joke) {
-    console.log(joke);
+app.get("/", function (req, res) {
+  res.setHeader("content-type", "text/html");
+  res.send("<h1 style='color: red'>Hello world from Express/!</h1>");
+});
+
+app.get("/:id", function (req, res) {
+  const query = req.query;
+  console.log(query);
+    res.setHeader("content-type", "text/html");
+    res.send("<h1 style='color: red'>Hello world from Express POST API call/!</h1>");
+});
+
+app.delete("/:id", function(req, res) {
+  res.sendStatus(201);
+  res.send("<h1 style='color: red'>Delete API call/!</h1>");
+})
+
+app.get("/hello", function(req, res) {
+    res.send("<h1 style='color: purple'>Hello world!</h1>");
+});
+
+app.get("/hello2", function(req, res) {
+    res.send("<h1 style='color: yellow'>Hello world!</h1>");
+});
+
+app.get("/hello3", function(req, res) {
+    res.send("<h1 style='color: green'>Hello world!</h1>");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, function () {
+  console.log("Server is running on port " + PORT);
 });
